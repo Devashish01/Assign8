26f56a7d92f6c4e0da4f131f382ecdd27a87fb44
Devashish01 <malhotradevashish01@gmail.com>
2018-06-15 02:00:56 +0530

    assignment 8 finally

--- Removevowel.java
+++ Removevowel.java
@@ -6,9 +6,18 @@ class B
 	{
 		 System.out.println("Enter a string pls");
 			Scanner input=new Scanner(System.in);
-			String m=input.nextLine();
-	        String n =m.replaceAll("[AEIOUaeiou]","");
-	        System.out.println(n);
+			String s1=input.nextLine();
+			for(int i=0;i<s1.length();i++)
+	        {
+	        	if(s1.charAt(i)=='a'||s1.charAt(i)=='e'||s1.charAt(i)=='i'||s1.charAt(i)=='o'||s1.charAt(i)=='u'||s1.charAt(i)=='A'||s1.charAt(i)=='E'||s1.charAt(i)=='I'||s1.charAt(i)=='O'||s1.charAt(i)=='U')
+	        	{	
+	        	}
+	        	else
+	        	{
+	        		System.out.print(s1.charAt(i));
+	        	}
+	        }  
+	    
 	}
 }
 public class Removevowel {
